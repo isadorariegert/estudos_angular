ecb3c5faaa95856868f5bc0fdfb9f7c667313698
Isadora Riegert <isadora.riegert@gmail.com>
2021-07-12 08:17:57 -0300

    saving progress

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,6 +1,18 @@
+//import { ServerComponent } from './server.component';
+// Um component é simplesmente uma typescript class que o Angular é capaz de instanciar para criar objetos com base no projeto que você está criando;
+//o decorator @Component não é algo que o typescript conhece
 import { Component } from '@angular/core';
 
-@Component()
+//decorator: aqui avisa ao angular que a classe abaixo não é apenas uma classe, mas um component;
+@Component({
+  selector: 'app-server',
+  //Os templates são obrigatórios no component.ts
+  //ao inves de inserir um arquivo html, é possível inserir direto o código ou o selector do component que você quer exibir como template.
+  templateUrl: './server.component.html'
+})
 export class ServerComponent {
-
+  serverID: number = 10;
+  serverStatus: string = "online";
 }
+
+
